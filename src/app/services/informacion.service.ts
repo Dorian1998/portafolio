f7e142835d2bedb155a39839820eb18be3c9b58f
dorian lenis <dorian.315@hotmail.com>
2017-08-17 20:41:06 -0500

    servicio firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,15 +6,32 @@ import { Http } from '@angular/http';
 export class InformacionService {
 
   info: any = {};
-  cargada:boolean = false;
+  cargada: boolean = false;
+  cargada_sobre_nosotros: boolean = false;
+  equipo:any [] = [];
 
   constructor(public http: Http) {
+
+      this.carga_info();
+      this.carga_sobre_nosotros();
+  }
+  public carga_info() {
     this.http.get("assets/data/info.pagina.json")
       .subscribe(data => {
-        console.log(data.json());
-        this.cargada=true;
-        this.info=data.json();
+        // console.log(data.json());
+        this.cargada = true;
+        this.info = data.json();
       });
+
+  }
+
+  public carga_sobre_nosotros(){
+    this.http.get("https://paginaweb-84255.firebaseio.com/equipo.json")
+    .subscribe(data => {
+      console.log(data);
+      this.cargada_sobre_nosotros = true;
+      this.equipo = data.json();
+    });
 
   }
 
