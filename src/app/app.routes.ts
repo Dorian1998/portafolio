7e9c52f1aea35e29cbce7af005d802ff16830a01
dorian lenis <dorian.315@hotmail.com>
2017-08-19 16:08:13 -0500

    terminando app angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ const app_routes: Routes = [
     { path: 'about', component: AboutComponent},
     { path: 'item/:id', component: ItemComponent},
     { path: 'buscar/:termino', component: SearchComponent},
-    { path: '**', pathMatch: 'full', redirectTo: ''}
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, { useHash:true});
